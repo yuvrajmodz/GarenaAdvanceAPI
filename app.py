642ae40b8ef7cd31f891c58e2f16393a4af681c4
yuvrajmodz <ys8619050@gmail.com>
2024-11-06 19:23:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,5 +72,5 @@ def api():
         return 'developed by: @YuvrajMODZ'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
